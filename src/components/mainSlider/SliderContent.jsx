29349810b75f5ec9a3dc37e1697bfc78e5f0b952
AnGu9192 <49748201+AnGu9192@users.noreply.github.com>
2023-04-04 20:07:08 +0300

    Update SliderContent.jsx

--- src/components/mainSlider/SliderContent.jsx
+++ src/components/mainSlider/SliderContent.jsx
@@ -1,29 +1,24 @@
 import React from "react";
-import { motion } from "framer-motion";
 
 function SliderContent({ activeIndex, imageSlider }) {
   return (
-    <motion.section>
-      <motion.div>
+    <section>
+      <div>
       {imageSlider.map((slide, index) => (
-        <motion.div
-          drag="x"
+        <div
+        
           key={index}
           className={index === activeIndex ? "slides active" : "inactive"}
         >
-          <motion.div>
           <img className="slide-image" src={slide.urls} alt={slide.title} />
-
-          </motion.div>
-
           
           <h2 className="slide-title">{slide.title}</h2>
           <button className="slide-button">{slide.buttontext}</button>
-        </motion.div>
+        </div>
       ))}
-      </motion.div>
+      </div>
      
-    </motion.section>
+    </section>
   );
 }
 
