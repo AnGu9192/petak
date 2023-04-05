f0706ef43dff0c167a589510d0a4aa72ad792190
AnGu9192 <49748201+AnGu9192@users.noreply.github.com>
2023-04-05 16:06:50 +0300

    Update imageSlider.js

--- src/components/mainSlider/imageSlider.js
+++ src/components/mainSlider/imageSlider.js
@@ -2,8 +2,6 @@ import First from '../../assets/First.jpg'
 import Second from '../../assets/Second.jpg'
 import Third from '../../assets/Third.jpg'
 
-
-
 export default [
     {
         urls: First,
