b55d3cc9f1d178e76a63f9985d7ef2d00b8b9f25
AnGu9192 <49748201+AnGu9192@users.noreply.github.com>
2023-04-11 12:19:18 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,44 @@
-import { BrowserRouter as Router, Route, Routes,Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
-import './App.css'
+import "./App.css";
 
-import Header from './components/header/Header';
+import Header from "./components/header/Header";
 import Navbar from "./components/navbar/Navbar";
-import Slider from './components/mainSlider/Slider';
+import AllCategories from "./components/pages/AllCategories";
+import Aaaa from "./components/pages/submenu/Aaaa";
+import Bbbb from "./components/pages/submenu/Bbbb";
+import Jobs from "./components/pages/Jobs";
+import Service from "./components/pages/Service";
+import RealEstate from "./components/pages/RealEstate";
+import Electronics from "./components/pages/Electronics";
+import ClotingFashion from "./components/pages/ClotingFashion";
+import HomeGarden from "./components/pages/HomeGarden";
 
-import AllCategories from './components/pages/AllCategories';
-import Aaaa from './components/pages/submenu/Aaaa';
-import Bbbb from './components/pages/submenu/Bbbb';
-import Jobs from './components/pages/Jobs';
-import Service from './components/pages/Service';
-import RealEstate from './components/pages/RealEstate';
-import Electronics from './components/pages/Electronics';
-import ClotingFashion from './components/pages/ClotingFashion';
-import HomeGarden from './components/pages/HomeGarden';
-
-import Footer from './components/footer/Footer'; 
-import LogIn from './components/pages/LogIn';
-import SignUp from './components/pages/SignUp';
-
-
+import Footer from "./components/footer/Footer";
+import LogIn from "./components/pages/LogIn";
+import SignUp from "./components/pages/SignUp";
 
 function App() {
-
   return (
     <div className="App">
-        <Router>
         <Header />
-
-        <Navbar/>    
-        <Routes>        
-          <Route path="/AllCategories" element={<AllCategories />}/>
-          <Route path="/" element={<Jobs />}/>
-          <Route path="/Aaaa" element={<Aaaa />}/>
-          <Route path="/Bbbb" element={<Bbbb />}/>
-          <Route path="/Service" element={<Service />}/>
-          <Route path="/RealEstate" element={<RealEstate />}/>
-          <Route path="/Electronics" element={<Electronics />}/>
-          <Route path="/HomeGarden" element={<HomeGarden/>}/>
-          <Route path="/ClotingFashion" element={<ClotingFashion />}/>
-          <Route path="/LogIn" element={<LogIn />}/>
-          <Route path="/SignUp" element={<SignUp />}/>
-
+        <Navbar />
+        <Routes>
+          <Route path="/AllCategories" element={<AllCategories />} />
+          <Route path="/" element={<Jobs />} />
+          <Route path="/Aaaa" element={<Aaaa />} />
+          <Route path="/Bbbb" element={<Bbbb />} />
+          <Route path="Service"  element={<Service />} />
+          <Route path="/RealEstate" element={<RealEstate />} />
+          <Route path="/Electronics" element={<Electronics />} />
+          <Route path="/HomeGarden" element={<HomeGarden />} />
+          <Route path="/ClotingFashion" element={<ClotingFashion />} />
+          <Route path="/LogIn" element={<LogIn />} />
+          <Route path="/SignUp" element={<SignUp />} />
         </Routes>
-    
-      </Router>  
-      <Slider />
-       <Footer />  
-      </div>
-  )
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
